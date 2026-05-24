43b1ef316597f97e40c736e34ecc75d896592fe9
PiotrCodi <151371404+PiotrCDV@users.noreply.github.com>
2026-05-24 15:58:45 +0200

    Revert "Scripts Update"
    This reverts commit e1f964b1044bc2bb944558ffb024c18b8d0c8aa0.

--- Assets/Scripts/Player/PlayerDodge.cs
+++ Assets/Scripts/Player/PlayerDodge.cs
@@ -1,10 +1,3 @@
-        inputActions.Player.Dodge.performed += ctx =>
-        {
-            if (ctx.control.device is Keyboard)
-            {
-                PrepareDodge();
-            }
-        };
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
